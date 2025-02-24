bc49bdb3741c787985f7b24394ef9eb200e633ec
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-02-24 10:33:33 +0100

    feat: correction name of the variables

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -11,21 +11,21 @@ console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = 'New York'.substring(4);
+const citySubstring = city.substring(4);
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = 'New York'.replace('York', 'Delhi');
+const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = 'New York ' + `${cityName}`;
+const cityNameConcat = city + ` ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = 'hello'.toUpperCase();
+const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
